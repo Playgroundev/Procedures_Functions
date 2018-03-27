d1a5f54adf6f9732a2d7a44745465e48a5264fbe
mtotodev05 <mtotodev05@gmail.com>
2018-03-27 15:40:14 +0300

    Initial Commit

---
+++ Procedures/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Location = New System.Drawing.Point(136, 25)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(166, 20)
+        Me.TxtUsername.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username"
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(136, 72)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtPassword.Size = New System.Drawing.Size(166, 20)
+        Me.TxtPassword.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(58, 136)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(119, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(183, 136)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(119, 23)
+        Me.BtnExit.TabIndex = 5
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(350, 261)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Name = "Form1"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TxtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TxtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents BtnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Procedures/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Private Sub btnLogin_Click(sender As System.Object, e As System.EventArgs) Handles btnLogin.Click
+        Dim User As String
+        Dim Password As String
+        User = TxtUsername.Text
+        Password = TxtPassword.Text
+        If User = "" Or Password = "" Then
+            MsgBox("Complete Fill in")
+            Return
+        End If
+        Call Login(User, Password)
+
+    End Sub
+    Sub Login(ByVal Username As String, ByVal Password As String)
+        If Username = "admin" And Password = "admin" Then
+            MsgBox("Correct Credentials")
+        Else
+            MsgBox("Incorrect Credentials")
+            TxtUsername.Text = ""
+            TxtPassword.Text = ""
+        End If
+    End Sub
+
+    Private Sub BtnExit_Click(sender As System.Object, e As System.EventArgs) Handles BtnExit.Click
+        Call Exiting()
+
+    End Sub
+
+
+
+    Sub Exiting()
+        Close()
+    End Sub
+
+End Class
